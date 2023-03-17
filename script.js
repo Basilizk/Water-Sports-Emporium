3ed8531e2bd843d31bec0165ad2b0cb15bd6ca34
Michael John Imbang <chronoire@root.local>
2023-03-17 15:09:59 -0400

    adding images

---
+++ script.js
@@ -0,0 +1,5 @@
+function featured() {
+  var container = document.querySelector(".featured");
+  container.style.cssText =
+    "display: block; width: 1400px; height:1000px; background-color: transparent; overflow: hidden; border: 0; position: relative; top: -80px; left: 420px;";
+}
