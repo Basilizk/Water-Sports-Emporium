b0200751e31f8b5a1ba5f0d9808318514c3eba42
Michael John Imbang <mjimban9@gmail.com>
2023-03-17 17:23:03 -0400

    adding more details

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function featured() {
   var container = document.querySelector(".featured");
   container.style.cssText =
-    "display: block; width: 1400px; height:1000px; background-color: transparent; overflow: hidden; border: 0; position: relative; top: -80px; left: 420px;";
+    "display: block; width: 1400px; height:1000px; background-color: transparent; overflow: hidden; border: 0; position: relative; top: -80px; left: 420px;animation: ease-out 10s forwards;";
 }
