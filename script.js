1a7ea15044075a47007acbf01af53adf97cde096
Michael John Imbang <mjimban9@gmail.com>
2023-03-17 22:41:06 -0400

    adding more products

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function featured() {
+function products() {
   var container = document.querySelector(".featured");
   container.style.cssText =
-    "display: block; width: 1400px; height:1000px; background-color: transparent; overflow: hidden; border: 0; position: relative; top: -80px; left: 420px;animation: ease-out 10s forwards;";
+    "display: block; width: 1400px; height:900px; background-color: transparent; overflow: hidden; border: 0; position: relative; top: -80px; left: 525px;";
 }
