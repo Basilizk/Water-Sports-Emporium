7e7c968cf18d6480ab0659176d913e960705e4fb
Michael John Imbang <mjimban9@gmail.com>
2023-03-18 19:51:12 -0400

    effects added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//iframe
 function products() {
   var container = document.querySelector(".featured");
   container.style.display = "block";
@@ -10,3 +11,29 @@ function products() {
   container.style.top = "-80px";
   container.style.left = "530px";
 }
+
+//button order
+const order = document.getElementById("order");
+
+order.addEventListener("click", function () {
+  alert("Thank you for your business.");
+});
+
+//button cancel
+const cancel = document.getElementById("cancel");
+
+cancel.addEventListener("click", function () {
+  alert("Sorry for not being able to do business with you.");
+});
+
+//input number limit
+const inputNumbers = document.querySelectorAll("input[type=number]");
+
+inputNumbers.forEach((input) => {
+  input.addEventListener("input", () => {
+    input.value = input.value.replace(/[^0-9]/g, "");
+    if (input.value.length > 3) {
+      input.value = input.value.slice(0, 3);
+    }
+  });
+});
